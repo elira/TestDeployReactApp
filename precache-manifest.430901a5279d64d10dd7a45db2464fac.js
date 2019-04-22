eeb8fa672a12043a3cd2567f27cfb15820e84907
elira <eli.rabinovitz@gmail.com>
2019-04-22 13:39:24 +0300

    Updates

---
+++ precache-manifest.430901a5279d64d10dd7a45db2464fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a34b871d4568f14891c",
+    "url": "/TestDeployReactApp/static/js/runtime~main.4a34b871.js"
+  },
+  {
+    "revision": "0cc9e69b831486f514df",
+    "url": "/TestDeployReactApp/static/js/main.0cc9e69b.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/TestDeployReactApp/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "0cc9e69b831486f514df",
+    "url": "/TestDeployReactApp/static/css/main.ad52ecde.chunk.css"
+  },
+  {
+    "revision": "e0166f11ccd4cc38c186f87e75df0dad",
+    "url": "/TestDeployReactApp/index.html"
+  }
+];
